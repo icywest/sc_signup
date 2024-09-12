c133199746de6f64d931b5d43ee84ea3832672d5
Patrick Solis <patrickresol@gmail.com>
2024-09-12 01:23:35 -0600

    button designed

--- src/client/Welcome.jsx
+++ src/client/Welcome.jsx
@@ -1,8 +1,12 @@
+import {Link} from "react-router-dom"
+
 function Welcome() {
     return(
         <>
         <main className="welcome-main">
-
+            <section>
+                <button className="btn btn-primary"><Link to={'/'}>Home</Link></button>
+            </section>
         </main>
         </>
     )
