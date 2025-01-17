94672cf6cc659c95189f4856c06b7d8ef52e67b8
Patrick Solis <patri@ASUS-Patrick.(none)>
2025-01-17 11:16:18 -0600

    Comment

---
+++ src/client/Comment.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import {useForm} from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+import "./App.css";
+
+function Comment() {
+
+}
+
+export default Comment;
